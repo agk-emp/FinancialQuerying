71a4d3c550d86ee2ae76f38464ed9a7bace76e4f
malikasp <7abdalghanikattan@gmail.com>
2024-10-02 13:46:35 +0300

    Created a diagram to visualize my database

--- CreditCardCompany.sql
+++ CreditCardCompany.sql
@@ -42,43 +42,56 @@
 -- 	REFERENCES card_number(id)
 -- );
 
-INSERT INTO country (country_name)
-VALUES 
-('United States'),
-('Canada'),
-('Mexico'),
-('Germany'),
-('Japan');
+-- INSERT INTO country (country_name)
+-- VALUES 
+-- ('United States'),
+-- ('Canada'),
+-- ('Mexico'),
+-- ('Germany'),
+-- ('Japan');
 
-INSERT INTO card_type (card_type_name)
-VALUES 
-('Visa'),
-('MasterCard'),
-('Amex'),
-('Discover'),
-('UnionPay');
+-- INSERT INTO card_type (card_type_name)
+-- VALUES 
+-- ('Visa'),
+-- ('MasterCard'),
+-- ('Amex'),
+-- ('Discover'),
+-- ('UnionPay');
 
-INSERT INTO customer (nin, first_name, last_name, country_id)
-VALUES 
-(123456789, 'John', 'Doe', 6),
-(987654321, 'Jane', 'Smith', 7),
-(456789123, 'Carlos', 'Mendez', 6),
-(321654987, 'Hans', 'Mueller', 8),
-(789456123, 'Yuki', 'Tanaka', 9);
+-- INSERT INTO customer (nin, first_name, last_name, country_id)
+-- VALUES 
+-- (123456789, 'John', 'Doe', 6),
+-- (987654321, 'Jane', 'Smith', 7),
+-- (456789123, 'Carlos', 'Mendez', 6),
+-- (321654987, 'Hans', 'Mueller', 8),
+-- (789456123, 'Yuki', 'Tanaka', 9);
 
-INSERT INTO card_number (card_number, customer_id, card_type_id)
-VALUES 
-(11112222, 11,6),
-(55556666, 12,7),
-(99990000, 13,7),
-(33334444, 14, 8),
-(77778888, 15, 8);
+-- INSERT INTO card_number (card_number, customer_id, card_type_id)
+-- VALUES 
+-- (11112222, 11,6),
+-- (55556666, 12,7),
+-- (99990000, 13,7),
+-- (33334444, 14, 8),
+-- (77778888, 15, 8);
 
-INSERT INTO card_transaction (date, amount, card_number_id)
-VALUES 
-('2024-09-01', 150.75, 6),
-('2024-09-02', 300.50, 7),
-('2024-09-03', 450.25, 8),
-('2024-09-04', 75.99, 8),
-('2024-09-05', 1200.00, 10);
+-- INSERT INTO card_transaction (date, amount, card_number_id)
+-- VALUES 
+-- ('2024-09-01', 150.75, 6),
+-- ('2024-09-02', 300.50, 7),
+-- ('2024-09-03', 450.25, 8),
+-- ('2024-09-04', 75.99, 8),
+-- ('2024-09-05', 1200.00, 10);
+
+
+
+-- SELECT EXTRACT(YEAR FROM date),
+-- EXTRACT (QUARTER FROM date),
+-- COUNT(amount)
+-- FROM card_transaction
+-- GROUP BY EXTRACT(YEAR FROM date),
+-- EXTRACT (QUARTER FROM date)
+-- ORDER BY EXTRACT(YEAR FROM date),
+-- EXTRACT (QUARTER FROM date);
+
+
 
